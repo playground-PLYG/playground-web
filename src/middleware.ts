f56222c62a843c1e7ff404bad6576e3a36871b6c
hsm0711 <bingo8@nate.com>
2023-08-14 17:49:27 +0900

    middleware url 패턴 변경

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 export const config = {
-  matcher: '/:path*',
+  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
 };
 
 export function middleware(request: NextRequest) {
