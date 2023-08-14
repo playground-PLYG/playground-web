18e82352aca53725f5ba2af2bd5ea9895005a8ef
hsm0711 <bingo8@nate.com>
2023-08-14 16:09:57 +0900

    format 적용

--- src/utils/common.ts
+++ src/utils/common.ts
@@ -1,9 +1,9 @@
 const commUtils = {
   /**
    * 쿼리 스트링 문자열을 반환한다.
-   * 
+   *
    * @param {Record<string, string>} obj
-   * 
+   *
    * @returns {string} 쿼리 스트링 문자열
    */
   getQueryString: (obj: Record<string, string>): string => {
@@ -11,10 +11,10 @@ const commUtils = {
   },
   /**
    * 페이징 처리를 위한 페이지 쿼리 스트링 문자열을 반환한다.
-   * 
+   *
    * @param {number} page 페이지 번호
    * @param {number} size 페이지당 row 수
-   * 
+   *
    * @returns {string} 쿼리 스트링 문자열
    */
   getPageQueryString: (page: number, size: number): string => {
