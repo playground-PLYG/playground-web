c2b5d7602e0a2e1f18dcc07a313bcfc2467d8325
kimsoohwan <ubsno4012@gmail.com>
2023-09-15 09:48:07 +0900

    login check

--- src/app/kakaoLogout1/page.tsx
+++ src/app/kakaoLogout1/page.tsx
@@ -7,12 +7,34 @@ import Typography from '@mui/material/Typography'
 import { useRouter } from 'next/navigation'
 export default function KakaoLogout() {
   const router = useRouter()
- 
-    const CLIENT_ID = '7e7b63658cacaa73e2a412dcd713bb8a';
-    const LOGOUT_REDIRECT_URI = new URL(document.location.origin) + '/kakaoLogout';
-    const kakaoURL = `https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`;
+  /**
+  const ACCESS_TOKEN = typeof window !== 'undefined' ? sessionStorage.getItem('TOKEN') : null
+  console.log('ACCESS_TOKEN :: ',ACCESS_TOKEN)
+    
+  if(!ACCESS_TOKEN){
+    window.alert('로그인을 먼저 해주세요')
+  } else{
+    const CLIENT_ID = '7e7b63658cacaa73e2a412dcd713bb8a'
+    const LOGOUT_REDIRECT_URI = new URL(document.location.origin) + '/kakaoLogout'      
+    const kakaoURL = `https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`
     router.push(kakaoURL)
- 
+  }
+   */
+
+  React.useEffect(() => {
+    const ACCESS_TOKEN = sessionStorage.getItem('TOKEN') 
+    console.log('ACCESS_TOKEN :: ',ACCESS_TOKEN)
+      
+    if(ACCESS_TOKEN){
+      window.alert('로그인을 먼저 해주세요')
+    } else{
+      const CLIENT_ID = '7e7b63658cacaa73e2a412dcd713bb8a'
+      const LOGOUT_REDIRECT_URI = new URL(document.location.origin) + '/kakaoLogout'      
+      const kakaoURL = `https://kauth.kakao.com/oauth/logout?client_id=${CLIENT_ID}&logout_redirect_uri=${LOGOUT_REDIRECT_URI}`
+      router.push(kakaoURL)
+    }
+  }, [])
+
   return (
     <Container>
       <Box
@@ -24,9 +46,9 @@ export default function KakaoLogout() {
         }}
       >
         <Typography variant="body1" gutterBottom>
-          test
+          로그인을 먼저 해주세요 :-)
         </Typography>
       </Box>
     </Container>
-  );
+  )
 }
